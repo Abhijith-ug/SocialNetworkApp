2ff8be621a04190c5c13a43cd79dd91561fab2c6
Abhijith-ug <abhijithug@gmail.com>
2023-05-15 10:23:52 +0530

    First Commit

---
+++ app/src/main/java/com/abhijith/socialnetworkapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.abhijith.socialnetworkapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFF8F5F5)
+val TextWhite = Color(0xFF000000)
+val BlueAccent = Color(0xFF36DED3)
+val HintGray  = Color(0xFF544E4E)
+val MediumGray = Color(0xFFF2EDED)
